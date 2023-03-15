ac2bd0edf6457b9cc73ce32d2934758d3a2d561a
William Russell <williamrussell@Williams-MBP.lan>
2023-03-14 23:29:23 -0400

    completed lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    num_list2 = [n for n in num_list if n % 2 == 0]
+    return num_list2
 
 def make_exclamation(sentence_list):
-    pass
+    sentence_list2 = [str + "!" for str in sentence_list]
+    return sentence_list2
